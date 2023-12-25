987cfec26c56ff4497989bcfbe4c6ad1d695005e
Kezuv <armin.foucek@gmail.com>
2023-12-25 22:10:22 +0100

    edit / clean up GameSession-Files

--- src/main/java/at/onlyquiz/controller/GameSessionController.java
+++ src/main/java/at/onlyquiz/controller/GameSessionController.java
@@ -17,6 +17,8 @@ import java.io.IOException;
 public class GameSessionController {
 
     private GameMode currentGameMode;
+    private Answer selectedAnswer;
+    private Button selectedAnswerButton;
     @FXML
     private Label totalScoreLabel, achievableScoreLabel, timeLabel, questionCounterLabel, questionLabel;
 
@@ -30,128 +32,159 @@ public class GameSessionController {
     private Button fiftyFiftyJokerButton, audienceJokerButton, chatJokerButton;
 
     @FXML
-    private Button answerAButton, answerBButton, answerCButton, answerDButton;
+    private Button answerAButton, answerBButton, answerCButton, answerDButton, commitButton;
 
     public GameSessionController() {
     }
 
     private void initialize() {
-        System.out.println("init function from GameSession Controller");
-        System.out.println(currentGameMode);
+        setJokersAvailability();
+        setScoreLabelsVisible();
+        setTimeVisible();
 
-        setScoreLabelsVisible(currentGameMode.isScoreVisible());
-        setTimeVisible(currentGameMode.isTimerVisible());
-        setJokersAvailability(currentGameMode.areJokersAvailable());
-
-        if (currentGameMode.isEditAble()){
-            setUpEditMode();
+        if (currentGameMode.isEditAble()) {
+            startEditView();
         } else {
-            setUpQuestionLabels();
+            startAnswerView();
         }
 
     }
 
-    public void setTimeVisible(boolean visible) {
-        timeLabel.visibleProperty().set(visible);
+
+    // Setup for answering questions
+
+    private void startAnswerView() {
+        setUpQuestionLabels();
     }
 
-    public void useFiftyFiftyJoker(ActionEvent actionEvent) {
-        if (!currentGameMode.getFiftyFiftyJokers().isEmpty()) {
-            currentGameMode.getFiftyFiftyJokers().pop().use(currentGameMode.getCurrentQuestion());
-        }
-    }
-
-    public void useAudienceJoker(ActionEvent actionEvent) {
-        if (!currentGameMode.getAudienceJokers().isEmpty()) {
-            currentGameMode.getAudienceJokers().pop().use(currentGameMode.getCurrentQuestion());
-        }
-    }
-
-    public void useChatJoker(ActionEvent actionEvent) {
-        if (!currentGameMode.getChatJokers().isEmpty()) {
-            currentGameMode.getChatJokers().pop().use(currentGameMode.getCurrentQuestion());
-        }
-    }
-
-
-    public void commitAnswer(Answer answer, Button answerButton) {
-        if (answer.isCorrect()) {
-            answerButton.getStyleClass().add("answer-button-right");
-        } else {
-            answerButton.getStyleClass().add("answer-button-wrong");
-        }
-    }
-
-    public void selectAnswerA(ActionEvent actionEvent) {
-        commitAnswer(currentGameMode.getCurrentQuestion().getSpecificAnswer(0), answerAButton);
-    }
-
-    public void selectAnswerB(ActionEvent actionEvent) {
-        commitAnswer(currentGameMode.getCurrentQuestion().getSpecificAnswer(1), answerBButton);
-    }
-
-    public void selectAnswerC(ActionEvent actionEvent) {
-        commitAnswer(currentGameMode.getCurrentQuestion().getSpecificAnswer(2), answerCButton);
-    }
-
-    public void selectAnswerD(ActionEvent actionEvent) {
-        commitAnswer(currentGameMode.getCurrentQuestion().getSpecificAnswer(3), answerDButton);
-    }
-
-    public void setUpQuestionLabels() {
+    private void setUpQuestionLabels() {
         answerATextField.setVisible(false);
         answerBTextField.setVisible(false);
         answerCTextField.setVisible(false);
         answerDTextField.setVisible(false);
         questionTextField.setVisible(false);
+        commitButton.disableProperty().set(true);
 
-        questionLabel.setText(currentGameMode.getCurrentQuestion().getQuestion());
-        answerAButton.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(0).getValue());
-        answerBButton.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(1).getValue());
-        answerCButton.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(2).getValue());
-        answerDButton.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(3).getValue());
+        freshUpQuestionLabels();
     }
 
-    public void setUpEditMode(){
+    public void freshUpQuestionLabels() {
+        questionLabel.setText(currentGameMode.getCurrentQuestion().getQuestion());
+        setUpAnswerButtonText(answerAButton, 0);
+        setUpAnswerButtonText(answerBButton, 1);
+        setUpAnswerButtonText(answerCButton, 2);
+        setUpAnswerButtonText(answerDButton, 3);
+    }
+
+    private void setUpAnswerButtonText(Button button, int index) {
+        button.getStyleClass().removeAll("answer-button-wrong");
+        button.getStyleClass().removeAll("answer-button.right");
+        button.getStyleClass().add("answer-button");
+        button.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(index).getValue());
+    }
+
+
+    // Setup for editing questions
+
+    private void startEditView() {
+        setUpEditMode();
+    }
+
+    public void setUpEditMode() {
+        commitButton.setVisible(false);
+        commitButton.disableProperty().set(true);
+
         answerAButton.setVisible(false);
         answerBButton.setVisible(false);
         answerCButton.setVisible(false);
         answerDButton.setVisible(false);
         questionLabel.setVisible(false);
 
-        if (!currentGameMode.getCurrentQuestion().getQuestion().isEmpty()){
+        freshUpTextFields();
+    }
+
+    private void freshUpTextFields() {
+        if (!currentGameMode.getCurrentQuestion().getQuestion().isEmpty()) {
             questionTextField.setText(currentGameMode.getCurrentQuestion().getQuestion());
         }
-
-        if (!currentGameMode.getCurrentQuestion().getSpecificAnswer(0).getValue().isEmpty()){
-            answerATextField.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(0).getValue());
-        }
-        if (!currentGameMode.getCurrentQuestion().getSpecificAnswer(1).getValue().isEmpty()){
-            answerBTextField.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(1).getValue());
-        }
-        if (!currentGameMode.getCurrentQuestion().getSpecificAnswer(3).getValue().isEmpty()){
-            answerCTextField.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(2).getValue());
-        }
-        if (!currentGameMode.getCurrentQuestion().getSpecificAnswer(3).getValue().isEmpty()){
-            answerDTextField.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(2).getValue());
-        }
-
+        setUpTextField(answerATextField, 0);
+        setUpTextField(answerBTextField, 1);
+        setUpTextField(answerCTextField, 2);
+        setUpTextField(answerDTextField, 3);
     }
 
-    public void setJokersAvailability(boolean availability) {
-        audienceJokerButton.setVisible(availability);
-        chatJokerButton.setVisible(availability);
-        fiftyFiftyJokerButton.setVisible(availability);
+    private void setUpTextField(TextField textField, int index) {
+        if (!currentGameMode.getCurrentQuestion().getSpecificAnswer(index).getValue().isEmpty()) {
+            textField.setText(currentGameMode.getCurrentQuestion().getSpecificAnswer(index).getValue());
+        }
     }
 
-    public void setScoreLabelsVisible(boolean visible) {
-        totalScoreLabel.visibleProperty().set(visible);
-        achievableScoreLabel.visibleProperty().set(visible);
+
+    //Basic functions for UI
+
+    public void useFiftyFiftyJoker() {
+        if (!currentGameMode.getFiftyFiftyJokers().isEmpty()) {
+            currentGameMode.getFiftyFiftyJokers().pop().use(currentGameMode.getCurrentQuestion());
+        }
     }
 
-    public void setCurrentGameMode(GameMode currentGameMode) {
-        this.currentGameMode = currentGameMode;
-        initialize();
+    public void useAudienceJoker() {
+        if (!currentGameMode.getAudienceJokers().isEmpty()) {
+            currentGameMode.getAudienceJokers().pop().use(currentGameMode.getCurrentQuestion());
+        }
+    }
+
+    public void useChatJoker() {
+        if (!currentGameMode.getChatJokers().isEmpty()) {
+            currentGameMode.getChatJokers().pop().use(currentGameMode.getCurrentQuestion());
+        }
+    }
+
+    public void selectAnswerA() {
+        selectSpecificAnswer(0, answerAButton);
+    }
+
+    public void selectAnswerB() {
+        selectSpecificAnswer(1, answerBButton);
+    }
+
+    public void selectAnswerC() {
+        selectSpecificAnswer(2, answerCButton);
+    }
+
+    public void selectAnswerD() {
+        selectSpecificAnswer(3, answerDButton);
+    }
+
+    private void selectSpecificAnswer(int index, Button answerButton) {
+        if (selectedAnswerButton != null) {
+            selectedAnswerButton.getStyleClass().removeAll("answer-button-selected");
+            selectedAnswerButton.getStyleClass().add("answer-button");
+        }
+
+        selectedAnswer = currentGameMode.getCurrentQuestion().getSpecificAnswer(index);
+        selectedAnswerButton = answerButton;
+        selectedAnswerButton.getStyleClass().add("answer-button-selected");
+        commitButton.disableProperty().set(false);
+    }
+
+    public void pressCommitButton() {
+        // When Question is not editable the answer will be committed
+        if (!currentGameMode.isEditAble()) {
+            if (selectedAnswer != null && selectedAnswerButton != null) {
+                if (selectedAnswer.isCorrect()) {
+                    selectedAnswerButton.getStyleClass().removeAll("answer-button-selected");
+                    selectedAnswerButton.getStyleClass().add("answer-button-right");
+                } else {
+                    selectedAnswerButton.getStyleClass().removeAll("answer-button-selected");
+                    selectedAnswerButton.getStyleClass().add("answer-button-wrong");
+                }
+            }
+        }
+        // When Question is editable, the input text will be saved
+        else {
+
+        }
     }
 
     public void pressStopButton() {
@@ -163,4 +196,28 @@ public class GameSessionController {
             throw new RuntimeException(e);
         }
     }
+
+
+    // Setter
+
+    public void setCurrentGameMode(GameMode currentGameMode) {
+        this.currentGameMode = currentGameMode;
+        initialize();
+    }
+
+    public void setJokersAvailability() {
+        audienceJokerButton.setVisible(currentGameMode.areJokersAvailable());
+        chatJokerButton.setVisible(currentGameMode.areJokersAvailable());
+        fiftyFiftyJokerButton.setVisible(currentGameMode.areJokersAvailable());
+    }
+
+    public void setScoreLabelsVisible() {
+        totalScoreLabel.visibleProperty().set(currentGameMode.isScoreVisible());
+        achievableScoreLabel.visibleProperty().set(currentGameMode.isScoreVisible());
+    }
+
+    public void setTimeVisible() {
+        timeLabel.visibleProperty().set(currentGameMode.isTimerVisible());
+    }
+
 }
