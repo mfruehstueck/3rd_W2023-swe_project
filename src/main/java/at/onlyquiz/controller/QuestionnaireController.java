a6e6508b8b29b1249416d489b54fdb1854f0d76d
Kezuv <armin.foucek@gmail.com>
2024-01-21 19:33:53 +0100

    finshed styling menu-view, scoreHistory-view.fxml, questionnaire-view.fxml

--- src/main/java/at/onlyquiz/controller/QuestionnaireController.java
+++ src/main/java/at/onlyquiz/controller/QuestionnaireController.java
@@ -35,6 +35,11 @@ public class QuestionnaireController extends BaseController implements Initializ
   @FXML
   public TextField ui_newQuestionnaire_textField;
 
+  private ObservableList<QuestionnaireEdit> questionnaire_obstList;
+  private ObservableList<GameQuestion> question_obstList;
+  private QuestionnaireEdit questionnaire_listView_selected;
+  private Integer question_listView_selectedIdx;
+
   //NSEC: instance members
   public QuestionnaireController() { super(); }
   @Override
@@ -48,10 +53,10 @@ public class QuestionnaireController extends BaseController implements Initializ
     fill_questionnaire_obstList();
 
     ui_questionnaire_listView.setItems(questionnaire_obstList);
-    ui_questionnaire_listView.setCellFactory(listView -> new QuestionnaireEditCell(onClick_update));
+    ui_questionnaire_listView.setCellFactory(listView -> new QuestionnaireEditCell(onClick_Export, onClick_editQuestionnaire, onClick_deleteQuestionnaire));
 
     ui_question_listView.setItems(question_obstList);
-    ui_question_listView.setCellFactory(listView -> new QuestionEditCell());
+    ui_question_listView.setCellFactory(listView -> new QuestionEditCell(onClick_editQuestion, onClick_deleteQuestion));
 
     ui_questionnaire_listView.getSelectionModel().selectedItemProperty().addListener((observable -> {
       questionnaire_listView_selected = ui_questionnaire_listView.getSelectionModel().getSelectedItem();
@@ -66,7 +71,7 @@ public class QuestionnaireController extends BaseController implements Initializ
   }
 
   //NSEC: eventHandlers
-  private final OnClickEventHandler<QuestionnaireEdit> onClick_update = (item) -> {
+  private final OnClickEventHandler<QuestionnaireEdit> onClick_Export = (item) -> {
     File destionationQuestionnaire = FileManagement.save_file("Export Questionnaire", new String[]{"csv Files", "*.csv"});
 
     if (destionationQuestionnaire == null) return;
@@ -75,11 +80,33 @@ public class QuestionnaireController extends BaseController implements Initializ
     FileManagement.copy_file(sourceQuestionnaire, destionationQuestionnaire);
   };
 
+  private final OnClickEventHandler<QuestionnaireEdit> onClick_editQuestionnaire = (item) -> {
+    questionnaire_listView_selected = item;
+    open_editMode(0);
+  };
+
+  private final OnClickEventHandler<QuestionnaireEdit> onClick_deleteQuestionnaire = (item) -> {
+    File questionnaire = QuestionDictionary.get_QuestionnaireFiles().get(item.getQuestionnaireName()).toFile();
+    FileManagement.delete_file(questionnaire);
+    ui_questionnaire_listView.getItems().remove(item);
+    QuestionDictionary.init();
+  };
+
+  private final OnClickEventHandler<GameQuestion> onClick_editQuestion = (item) -> {
+    question_listView_selectedIdx = ui_question_listView.getItems().indexOf(item);
+    open_editMode(question_listView_selectedIdx);
+  };
+
+  private final OnClickEventHandler<GameQuestion> onClick_deleteQuestion = (item) -> {
+    String selected_questionnaireName = questionnaire_listView_selected.getQuestionnaireName();
+    question_listView_selectedIdx = ui_question_listView.getItems().indexOf(item);
+    ui_question_listView.getItems().remove(item);
+
+    QuestionDictionary.delete_gameQuestion(selected_questionnaireName, item.getLineIdx());
+  };
+
   //NSEC: instance fields
-  private ObservableList<QuestionnaireEdit> questionnaire_obstList;
-  private ObservableList<QuestionEdit> question_obstList;
-  private QuestionnaireEdit questionnaire_listView_selected;
-  private Integer question_listView_selectedIdx;
+
 
   //NSEC: private methods
   private void fill_questionnaire_obstList() {
--- src/main/java/at/onlyquiz/controller/cells/QuestionnaireEditCell.java
+++ src/main/java/at/onlyquiz/controller/cells/QuestionnaireEditCell.java
@@ -12,20 +12,31 @@ public class QuestionnaireEditCell extends BaseCell<QuestionnaireEdit> {
   private final Button ui_rename_button;
   private final Button ui_export_button;
   private final HBox ui_layout;
+  private final Button editButton;
+  private final Button ui_delete_button;
 
 
-  public QuestionnaireEditCell(OnClickEventHandler<QuestionnaireEdit> onClick_ui_export_button) {
+  public QuestionnaireEditCell(OnClickEventHandler<QuestionnaireEdit> onClick_ui_export_button,
+                               OnClickEventHandler<QuestionnaireEdit> onClick_editButton,
+                               OnClickEventHandler<QuestionnaireEdit> onClick_delete_button) {
     super();
 
     ui_questionnaireName = new Label();
     ui_rename_button = new Button();
+
+
+    editButton = new Button();
+    editButton.setText("Edit");
+    editButton.getStyleClass().add("edit-button");
+    editButton.setOnAction(actionEvent -> onClick_editButton.onCLick(getItem()));
 
     ui_export_button = new Button();
     ui_export_button.setText("Export");
     ui_export_button.getStyleClass().add("export-button");
     ui_export_button.setOnAction(actionEvent -> onClick_ui_export_button.onCLick(getItem()));
 
-    ui_layout = new HBox(ui_questionnaireName, blank, ui_export_button);
+    ui_layout = new HBox(ui_questionnaireName, blank, editButton, ui_export_button, ui_delete_button);
+    ui_layout.setSpacing(10);
   }
 
   @Override
